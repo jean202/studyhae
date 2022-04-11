fce309322f9031d48ba2c859d7d6e76ea7add322
jean202 <hb13pud731@gmail.com>
2022-04-11 11:49:35 +0900

    sign-up.html, SignUpForm

--- src/main/java/com/studyhae/account/AccountController.java
+++ src/main/java/com/studyhae/account/AccountController.java
@@ -9,6 +9,10 @@ public class AccountController {
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model){
+
+//        이름이 camelCase로 같으면 굳이 name속성을 안써줘도 된다.
+//        model.addAttribute("signUpForm", new SignUpForm());
+        model.addAttribute(new SignUpForm());
         return "account/sign-up";
     }
 }
--- src/main/java/com/studyhae/config/SecurityConfig.java
+++ src/main/java/com/studyhae/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.studyhae.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -18,5 +20,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/email-login", "/check-email-login", "/login-link").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll() // 프로필은 왜 get으로는 인증 없이도 보여야 할까..?애초에 로그인 후 보여주는게 프로필 아니던가
                 .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
